109aa4e2846b746aa2964c58988aa57a55dea7fd
Gőcze Norbert <gnorbi951@gmail.com>
2020-07-13 22:32:49 +0200

    API Gateway can call VideoService

--- api-gateway/src/main/java/com/codecool/ap/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/codecool/ap/apigateway/ApiGatewayApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
