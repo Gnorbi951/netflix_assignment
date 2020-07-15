304aaa58b64eca155c39282eb14f5b1337c4b935
Gőcze Norbert <gnorbi951@gmail.com>
2020-07-15 14:35:49 +0200

    Narrow down crossorigin to localhost:3000

--- video-service/src/main/java/com/codecool/videoservice/security/SecurityConfig.java
+++ video-service/src/main/java/com/codecool/videoservice/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
