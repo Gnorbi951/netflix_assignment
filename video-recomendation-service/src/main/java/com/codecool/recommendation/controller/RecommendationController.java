153344c9c807e8f971cd1be73a663060ba8502eb
Gőcze Norbert <gnorbi951@gmail.com>
2020-07-15 21:06:41 +0200

    Add Docket, CommandLineRunner and GetMapping endpoint to Controller

--- video-recomendation-service/src/main/java/com/codecool/recommendation/controller/RecommendationController.java
+++ video-recomendation-service/src/main/java/com/codecool/recommendation/controller/RecommendationController.java
@@ -3,6 +3,7 @@ package com.codecool.recommendation.controller;
 import com.codecool.recommendation.entity.Recommendation;
 import com.codecool.recommendation.service.RecommendationManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +16,8 @@ public class RecommendationController {
     @Autowired
     private RecommendationManager recommendationManager;
 
+    @GetMapping("/all")
     public List<Recommendation> getAll() {
-
-        return null;
+        return recommendationManager.getAll();
     }
 }
