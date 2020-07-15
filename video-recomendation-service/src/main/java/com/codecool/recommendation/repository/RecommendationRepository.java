2b4c8f6db71efcdc8ec9f17243636602b2407931
Gőcze Norbert <gnorbi951@gmail.com>
2020-07-15 20:57:56 +0200

    Add VideoRecommendationRepository.java

---
+++ video-recomendation-service/src/main/java/com/codecool/recommendation/repository/RecommendationRepository.java
@@ -0,0 +1,8 @@
+package com.codecool.recommendation.repository;
+
+import com.codecool.recommendation.entity.Recommendation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
+    
+}
