02c8bc4fd07e5382b8536da871e474e0e2b517b4
Gőcze Norbert <gnorbi951@gmail.com>
2020-07-16 22:30:43 +0200

    Add style

--- netflix_frontend/src/Components/VideoDetail.js
+++ netflix_frontend/src/Components/VideoDetail.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import Recommendation from "./Recommendation";
+import styled from "styled-components";
 
 const VideoDetail = (props) => {
     const [details, setDetails] = useState();
@@ -8,7 +9,9 @@ const VideoDetail = (props) => {
 
     useEffect(() => {
         axios.get(`http://localhost:8762/videoservice/video-controller/video/${props.match.params.id}`)
-            .then(response => setVideo(response));
+            .then(response => {
+                console.log(response)
+                setVideo(response)});
 
 
         axios.get(`http://localhost:8762/videoservice/video-controller/video-with-recommendations/${props.match.params.id}`)
@@ -20,20 +23,28 @@ const VideoDetail = (props) => {
     return (
 
         <React.Fragment>
-            <div className={"video"}>
+            <VideoContainer>
             {video ? <iframe
+                             width={"100%"}
+                             height={"100%"}
                              frameBorder={0}
                              allow={"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"}
                              allowFullScreen={true}
                              title={video.id}
-                             src={`https://youtube.com/embed/${video.viewKey}`} />
+                             src={`https://youtube.com/embed/${video.data.viewKey}`} />
                              : <h1>Loading</h1>}
-            </div>
+            </VideoContainer>
             {details ? <Recommendation recommendations={details} /> : <h2>No Recommendations</h2>}
         {}</React.Fragment>
 
     );
 
 }
+const VideoContainer = styled.div`
+    margin: 2rem;
+    height: 30rem;
+    padding: 5px;
+`;
+
 
 export default VideoDetail;
