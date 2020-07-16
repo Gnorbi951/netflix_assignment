d696d6bf9eb7ed2644fc8702dc8af3b49f5f11c0
Gőcze Norbert <gnorbi951@gmail.com>
2020-07-16 22:18:46 +0200

    Display comment and rating

--- netflix_frontend/src/Components/Recommendation.js
+++ netflix_frontend/src/Components/Recommendation.js
@@ -5,12 +5,12 @@ const Recommendation = (props) => {
     console.log(props.recommendations.data);
     return (
         <React.Fragment>
-            {
-                props.recommendations.data.map((detail) =>
-                <p>{detail.comment}</p>
-
-                )
-            }
+            {props.recommendations.data.map((detail) => (
+                <div key={detail.id}>
+                    <p>{detail.comment}</p>
+                    <p>Rating: {detail.rating}/10</p>
+                </div>
+            ))}
         </React.Fragment>
     );
 }
